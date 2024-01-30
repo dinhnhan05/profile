8c7c3ad599366a59abb19295e7255dbbd1b6ff26
Nguyễn Đình Nhân <132151291+dinhnhan05@users.noreply.github.com>
2024-01-30 22:18:10 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   username: 'dinhnhan05',//thay thế username github của bạn vào đây
-  repoName: 'MyProfileWeb',//thay thế repo muốn hiện stars và lượt forks github vào đây
+  repoName: 'profile',//thay thế repo muốn hiện stars và lượt forks github vào đây
 };
 
 export default config;
